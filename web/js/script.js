0cd6507c6633bc92baff35865077d46b908a5354
Alexander ▲▼ <106950250+Alexander7000@users.noreply.github.com>
2022-10-08 19:34:52 -0600

    Delete web directory

--- web/js/script.js
+++
@@ -1,7 +0,0 @@
-let form = document.forms.formbusca;
-
-const changeInput = () => {
-    document.querySelector("#buscar").disabled = false;
-    if (form.titulo.value == "" && form.escritor.value == "")
-        document.querySelector("#buscar").disabled = true;
-}
